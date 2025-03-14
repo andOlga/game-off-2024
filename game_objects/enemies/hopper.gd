05b45b38e46b8007ce8a5c87fa4df732e2635d5c
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-14 16:33:27 +0400

    Fix enemy collisions

--- game_objects/enemies/hopper.gd
+++ game_objects/enemies/hopper.gd
@@ -1,10 +1,16 @@
 @icon("res://sprite_images/sBlob.png")
 
-extends Node2D
+extends CharacterBody2D
 
 @export var step_size := 32
 
 func _on_movement_timer_timeout() -> void:
 	var move_amount : int = [-1, 1].pick_random() * step_size
 	var move_direction : StringName = [&"x", &"y"].pick_random()
-	position[move_direction] += move_amount
+	velocity[move_direction] = move_amount * 3600
+	
+func _physics_process(delta: float) -> void:
+	velocity *= delta
+	if move_and_slide():
+		_on_movement_timer_timeout()
+	velocity = Vector2(0, 0)
