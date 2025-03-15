790cabeb09fb61d77fb7f3913c7b811aa48aef2f
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-15 20:30:22 +0400

    Seed input box (non-functioning)

--- title_screen.gd
+++ title_screen.gd
@@ -3,10 +3,11 @@ extends RichTextLabel
 var is_in_fade := false
 
 func _process(delta: float) -> void:
+	if not $SeedBox.has_focus():
+		$SeedBox.grab_focus()
 	if Input.is_action_just_pressed("action_pause") and not is_in_fade:
 		is_in_fade = true
 		$FadeTimer.start()
-
 
 func _on_fade_timer_timeout() -> void:
 	if modulate.a <= 0:
