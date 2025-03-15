08f50bdda869049df17c9205eb1e27226779289d
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-15 23:08:39 +0400

    eh

--- utilities/title_screen.gd
+++ utilities/title_screen.gd
@@ -9,6 +9,7 @@ func _process(delta: float) -> void:
 	if not $SeedBox.has_focus():
 		$SeedBox.grab_focus()
 	if Input.is_action_just_pressed("action_pause") and not is_in_fade:
+		is_in_fade = true
 		var new_seed = $SeedBox.text
 		if new_seed.length() > 0:
 			if new_seed.is_valid_int():
@@ -17,7 +18,6 @@ func _process(delta: float) -> void:
 				new_seed = hash(new_seed)
 			RoomRng.rng.seed = new_seed
 		$SeedBox.text = str(RoomRng.rng.seed)
-		is_in_fade = true
 		$FadeTimer.start()
 
 func _on_fade_timer_timeout() -> void:
