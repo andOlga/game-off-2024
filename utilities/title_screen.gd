5af4754e3ab64934b00e14f62be80ce0a1388782
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-15 22:38:31 +0400

    Implement RNG fully

--- utilities/title_screen.gd
+++ utilities/title_screen.gd
@@ -2,10 +2,17 @@ extends RichTextLabel
 
 var is_in_fade := false
 
+func _ready() -> void:
+	$SeedBox.text = str(RoomRng.rng.seed)
+	
 func _process(delta: float) -> void:
 	if not $SeedBox.has_focus():
 		$SeedBox.grab_focus()
 	if Input.is_action_just_pressed("action_pause") and not is_in_fade:
+		var new_seed : String = $SeedBox.text
+		if new_seed.length() > 0:
+			RoomRng.rng.seed = hash(new_seed)
+		$SeedBox.text = str(RoomRng.rng.seed)
 		is_in_fade = true
 		$FadeTimer.start()
 
