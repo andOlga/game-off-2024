4fa96ccc8b9009b074a0827ffdcccf6597f572a1
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-16 01:15:18 +0400

    Implement pause

---
+++ utilities/pause_screen.gd
@@ -0,0 +1,20 @@
+extends RichTextLabel
+
+func _ready() -> void:
+	hide()
+	get_tree().paused = false
+
+func _process(delta: float) -> void:
+	var scene_tree := get_tree()
+	if scene_tree.paused:
+		if Input.is_action_just_pressed("action_pause"):
+			hide()
+			scene_tree.paused = false
+		elif Input.is_action_just_pressed("action_select"):
+			scene_tree.quit()
+		elif Input.is_action_just_pressed("action_item"):
+			scene_tree.reload_current_scene()
+	else:
+		if Input.is_action_just_pressed("action_pause"):
+			scene_tree.paused = true
+			show()
