7d811d47c40d2c4ae321c2d59af2dad84f22f752
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-15 20:17:36 +0400

    Basic title screen impl (no seed yet)

---
+++ title_screen.gd
@@ -0,0 +1,15 @@
+extends RichTextLabel
+
+var is_in_fade := false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("action_pause") and not is_in_fade:
+		is_in_fade = true
+		$FadeTimer.start()
+
+
+func _on_fade_timer_timeout() -> void:
+	if modulate.a <= 0:
+		get_tree().change_scene_to_file("res://test_scene.tscn")
+	else:
+		modulate.a -= 0.1
