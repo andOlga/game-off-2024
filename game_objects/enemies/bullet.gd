0947e9e74fafc26aafc5695849d50ae9ea2a63f6
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-18 16:37:46 +0400

    fix warning

--- game_objects/enemies/bullet.gd
+++ game_objects/enemies/bullet.gd
@@ -2,7 +2,7 @@ extends BaseEnemy
 
 var is_moving := true
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if is_moving and move_and_collide(transform.x * 5):
 		is_moving = false
 		kill()
