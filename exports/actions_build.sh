42d90e1fb6e33d87d01a622f346de9e7c039097b
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-22 22:15:11 +0400

    Be more quiet for GH logs

--- exports/actions_build.sh
+++ exports/actions_build.sh
@@ -6,12 +6,12 @@ GODOT_ARCH="linux.x86_64"
 cd "$(dirname "$0")"
 bash clean.sh
 echo "Downloading editor..."
-wget "https://github.com/godotengine/godot/releases/download/${GODOT_VERSION}/Godot_v${GODOT_VERSION}_${GODOT_ARCH}.zip"
+wget -q "https://github.com/godotengine/godot/releases/download/${GODOT_VERSION}/Godot_v${GODOT_VERSION}_${GODOT_ARCH}.zip"
 echo "Downloading export templates..."
-wget "https://github.com/godotengine/godot/releases/download/${GODOT_VERSION}/Godot_v${GODOT_VERSION}_export_templates.tpz"
+wget -q "https://github.com/godotengine/godot/releases/download/${GODOT_VERSION}/Godot_v${GODOT_VERSION}_export_templates.tpz"
 echo "Unzipping..."
-unzip *.zip
-unzip *.tpz
+unzip -q *.zip
+unzip -q *.tpz
 echo "Installing templates..."
 mkdir -p ~/.local/share/godot/export_templates/
 mv templates ~/.local/share/godot/export_templates/${GODOT_VERSION/-/.}
