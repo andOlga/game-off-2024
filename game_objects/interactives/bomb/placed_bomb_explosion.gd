85013c3c17081b8e01edfd88bae4adc7901db87d
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-25 19:37:25 +0400

    Implement CrackedWall

--- game_objects/interactives/bomb/placed_bomb_explosion.gd
+++ game_objects/interactives/bomb/placed_bomb_explosion.gd
@@ -7,7 +7,8 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 		body.hurt()
 	elif body is BaseEnemy:
 		body.kill()
-	# TODO: Add CrackedWall
+	elif body is CrackedWall:
+		body.queue_free()
 
 func _on_vaporize_timer_timeout() -> void:
 	queue_free()
---
+++ game_objects/obstacles/cracked_wall.gd
@@ -0,0 +1,3 @@
+class_name CrackedWall extends StaticBody2D
+
+# This script intentionally left blank.
