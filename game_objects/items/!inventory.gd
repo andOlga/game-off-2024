943d2c52b1f02611caca2196b7a49311f9136396
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-28 19:18:04 +0400

    Optimize inventory & flash item on pickup

--- game_objects/items/!inventory.gd
+++ game_objects/items/!inventory.gd
@@ -3,28 +3,33 @@
 extends Node2D
 
 var items : Array[BaseItem] = []
-var current_item = 0
+var item_size := 0
+var current_item := 0
 
 func _ready() -> void:
 	$CurrentItemSprite.hide()
 
 func add_item(item: BaseItem) -> void:
 	items.append(item)
+	item_size += 1
+	current_item = item_size - 1
+	_flash_current_item()
+
+func _flash_current_item() -> void:
+	$CurrentItemSprite.texture = items[current_item].get_node("Sprite").texture
+	$CurrentItemSprite.show()
+	$SpriteHideTimer.start()
 
 func _process(_delta: float) -> void:
-	var size := items.size()
-	if size == 0:
+	if item_size == 0:
 		return
 	if Input.is_action_just_pressed(&"action_select"):
 		current_item += 1
-		if current_item > size - 1:
+		if current_item > item_size - 1:
 			current_item = 0
-		$CurrentItemSprite.texture = items[current_item].get_node("Sprite").texture
-		$CurrentItemSprite.show()
-		$SpriteHideTimer.start()
+		_flash_current_item()
 	elif Input.is_action_just_pressed(&"action_item"):
 		items[current_item].use()
-
 
 func _on_sprite_hide_timer_timeout() -> void:
 	$CurrentItemSprite.hide()
