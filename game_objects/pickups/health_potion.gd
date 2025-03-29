cf4fb05773881030763df09f501a0010458cd036
&Olga <462484+andOlga@users.noreply.github.com>
2025-03-29 17:57:51 +0400

    Re-add timeout to InvulnPotion

--- game_objects/pickups/health_potion.gd
+++ game_objects/pickups/health_potion.gd
@@ -3,6 +3,5 @@
 extends BasePickup
 
 func activate(activator: Player) -> void:
-	if not activator.is_invuln:
-		activator.hp = 3
-		activator.update_health_display()
+	activator.hp = 3
+	activator.update_health_display()
--- game_objects/pickups/invuln_potion.gd
+++ game_objects/pickups/invuln_potion.gd
@@ -2,6 +2,24 @@
 
 extends BasePickup
 
+@export var duration_seconds := 10.0
+var stored_activator : Player
+
+func _ready() -> void:
+	$InvulnTimer.wait_time = duration_seconds
+
 func activate(activator: Player) -> void:
 	activator.is_invuln = true
-	activator.get_node("Sprite").modulate = Color("00FFFF")
+	activator.get_node("InvisBlinkTimer").start()
+	stored_activator = activator
+	$InvulnTimer.start()
+
+func _on_pickup_sfx_finished() -> void:
+	pass # Do not instantly destroy this pickup
+
+func _on_invuln_timer_timeout() -> void:
+	stored_activator.is_invuln = false
+	stored_activator.get_node("InvisBlinkTimer").stop()
+	stored_activator.get_node("Sprite").modulate.a = 1
+	stored_activator.update_health_display()
+	queue_free()
