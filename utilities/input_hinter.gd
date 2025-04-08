4fb3890d9f4c09d1caa9162124f09a09c5a9cc16
&Olga <462484+andOlga@users.noreply.github.com>
2025-04-08 09:29:20 +0400

    Move input hints to a global

---
+++ utilities/input_hinter.gd
@@ -0,0 +1,16 @@
+extends Node
+
+func format_input_hint(text: String) -> String:
+	return text.format({
+		"dpad": "\u21ce",
+		"arrows": "\u23f4\u23f5\u23f6\u23f7",
+		"a": "\u21a7",
+		"key_z": "\uff3a",
+		"b": "\u21a6",
+		"key_x": "\uff38",
+		"select": "\u21f7",
+		"space": "\u243a",
+		"start": "\u21f8",
+		"enter": "\u242e",
+		"esc": "\u242f"
+	})
